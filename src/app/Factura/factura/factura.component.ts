27c4c053d43f8c36e436f79c39f283a71683f5d7
ItsAiz <andres.sierra03@uptc.edu.co>
2022-06-12 12:24:33 -0500

    avance 2

--- src/app/Factura/factura/factura.component.ts
+++ src/app/Factura/factura/factura.component.ts
@@ -3,6 +3,7 @@ import { Factura } from 'src/app/Modelo/Factura';
 import { ServiceService } from 'src/app/Service/service.service';
 import { Router } from '@angular/router';
 import { Detalle } from 'src/app/Modelo/Detalle';
+import { Cliente } from 'src/app/Modelo/Cliente';
 
 @Component({
   selector: 'app-factura',
@@ -11,23 +12,31 @@ import { Detalle } from 'src/app/Modelo/Detalle';
 })
 export class FacturaComponent implements OnInit {
   detalles:Detalle[];
+  clientes:Cliente[];
   facturaNueva: Factura ={id_factura:0, id_cliente:0, fecha:new Date('')};
   constructor(private service:ServiceService, private router:Router) { }
 
   ngOnInit(): void {
     this.listarDetalles();
+    this.obtenerClientes();
   }
   listarDetalles(){
     this.service.getDetalles().subscribe(data=>{
       this.detalles = data;
-      console.log(data);
     });
   }
 
   buscarFactura(numb:number){
       this.service.getFactura(numb).subscribe(data=>{
-          this.facturaNueva = data;
+          this.facturaNueva.id_factura = data.id_factura;
+          this.facturaNueva.id_cliente = data.id_cliente;
+          this.facturaNueva.fecha = new Date(data.fecha);
       });
+  }
+  obtenerClientes(){
+    this.service.getClientes().subscribe(data=>{
+        this.clientes = data;
+    });
   }
 
 }
